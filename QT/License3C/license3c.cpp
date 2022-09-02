bd662b41294abc78aacf74cc95d0aa49bae2f433
Pai Peng <sipaipv6@gmail.com>
2022-09-02 15:02:23 +0800

    add structure for license

--- QT/License3C/license3c.cpp
+++ QT/License3C/license3c.cpp
@@ -12,12 +12,19 @@ bool License3C::checkMagicBytes(const QByteArray magicByte) {
             ((unsigned char)magicByte.at(2) == 0x4E) &&
             ((unsigned char)magicByte.at(3) == 0x5E));
 }
-void License3C::parseLicense(const QByteArray& byteArray) {
+License* License3C::parseLicense(const QByteArray& byteArray) {
     qDebug() << "parseLicense: " << byteArray;
+    License* license = NULL;
     QByteArray magicBytes = byteArray.mid(0, 4);
     if (checkMagicBytes(magicBytes)) {
         qDebug() << "magic bytes valid!";
+
+        // Feature length 4Bytes
+        // Feature type:  4 Bytes
+        // Name length: 4Bytes
+        // Value Length: 4Bytes optional, only for
     } else {
         qDebug() << "magic bytes invalid!";
     }
+    return license;
 }
--- QT/License3C/license3c.h
+++ QT/License3C/license3c.h
@@ -3,12 +3,37 @@
 
 #include "License3C_global.h"
 
+
+typedef struct _license {
+    int name_length;
+    char* name;
+    int value_length;
+    char* value;
+    struct _license* next;
+} License;
+
+
 class LICENSE3C_EXPORT License3C
 {
 public:
     License3C();
     static bool checkMagicBytes(const QByteArray magicByte);
-    static void parseLicense(const QByteArray& byteArray);
+    static License* parseLicense(const QByteArray& byteArray);
+
+    enum Type {
+        BINARY = 1, // VARIABLE_LENGTH
+        STRING = 2, // VARIABLE_LENGTH
+        BYTE = 3, // 1 byte
+        SHORT = 4, // 2 bytes
+        INT = 5, // 4 bytes
+        LONG = 6, // 8 bytes
+        FLOAT = 7, // 4 bytes
+        DOUBLE = 8, // 8 bytes
+        BIGINTEGER = 9, // VARIABLE_LENGTH
+        BIGDECIMAL = 10, // VARIABLE_LENGTH
+        DATE = 11, // Long.BYTES 8
+        UUID = 12 // 2 * Long.BYTES 16
+    };
 };
 
 #endif // LICENSE3C_H
